ba0e03f24693acd5da135bb7a1b65599bc729906
Robby Roda <robbyroda.00@gmail.com>
2025-09-22 22:45:55 +0800

    Robby Roda

--- src/app/pages/main/main.ts
+++ src/app/pages/main/main.ts
@@ -3,9 +3,10 @@ import { About } from "../about/about";
 import { Contact } from "../contact/contact";
 import { Navbar } from "../../shared/navbar/navbar";
 import { Sidebar } from "../../shared/sidebar/sidebar";
+import { Footer } from '../footer/footer';
 @Component({
   selector: 'app-main',
-  imports: [About, Contact, Navbar, Sidebar],
+  imports: [About, Contact, Navbar, Sidebar, Footer],
   templateUrl: './main.html'
 })
 export class Main {
